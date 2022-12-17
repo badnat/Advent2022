c9514673285ee4102e287bb76ef108eb32d00c3b
badnat <nathanielbadgett@gmail.com>
2022-12-17 18:51:01 -0500

    day1 finished, thanks for telling me about this denver

---
+++ day1/target/debug/deps/day1-2a8f60917d02b761.d
@@ -0,0 +1,5 @@
+/home/badnat/Code/advent2022/day1/target/debug/deps/day1-2a8f60917d02b761: src/main.rs
+
+/home/badnat/Code/advent2022/day1/target/debug/deps/day1-2a8f60917d02b761.d: src/main.rs
+
+src/main.rs:
